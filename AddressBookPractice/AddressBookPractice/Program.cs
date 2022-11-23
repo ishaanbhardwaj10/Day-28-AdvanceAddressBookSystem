1c816fa61a5a2a14c3169394ed9bed0b188d86ec
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-11-23 17:31:10 +0530

    added UC-14: Read and Write to CSV file

---
+++ AddressBookPractice/AddressBookPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AddressBookPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //UC-14
+            MultipleAddressBooks multipleAddressBooks = new MultipleAddressBooks();
+
+            //adding address books and contacts
+            multipleAddressBooks.addAddressBook();
+            multipleAddressBooks.addContact();
+            multipleAddressBooks.addAddressBook();
+            multipleAddressBooks.addContact();
+
+            //writing to csv file
+            multipleAddressBooks.WriteToCSVFile();
+
+            //reading from csv file
+            MultipleAddressBooks.ReadFromCsvFile();
+
+
+
+        }
+    }
+}
